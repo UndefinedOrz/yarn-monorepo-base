36547982b144250ae676b612674deb0634e5a9bb
Harry Tran <nhattq.coding@gmail.com>
2021-07-10 13:46:52 +0700

    :tada: init commit

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -1,9 +1,5 @@
-import styled from '@emotion/styled'
-import Button, { ButtonProps } from '@material-ui/core/Button'
+import { ButtonProps } from '@material-ui/core/Button'
+import { StyledButton } from './styles'
 import React from 'react'
-
-export const StyledButton = styled(Button)`
-  color: red;
-`
 
 export const RepButton: React.FC<ButtonProps> = (props) => <StyledButton {...props} />
